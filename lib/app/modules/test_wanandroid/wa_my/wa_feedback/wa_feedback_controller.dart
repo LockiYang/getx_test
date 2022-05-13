33ad8e0b715186716b016abd9ed7642404399e65
Locki <lockiyang@qq.com>
2022-05-13 22:06:22 +0800

    xl

--- lib/app/modules/test_wanandroid/wa_my/wa_feedback/wa_feedback_controller.dart
+++ lib/app/modules/test_wanandroid/wa_my/wa_feedback/wa_feedback_controller.dart
@@ -1,6 +1,50 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_test/app/utils/toast_util.dart';
+import 'package:image_picker/image_picker.dart';
 
 class WaFeedbackController extends GetxController {
+
+  List<File> photoEntity = [];
+  String issue = '';
+  String contact = '';
+
+  /// 打开相册
+  /// [index] 索引
+  openGallery(int index) async {
+    if (photoEntity.length >= 4) {
+      ToastUtil.show('最多选择4张哟');
+      return;
+    }
+    if (index == photoEntity.length) {
+      var image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image != null) {
+        photoEntity.add(File(image.path));
+      }
+      update();
+    }
+  }
+
+  ///由于没有反馈接口，因此延时2秒进行操作
+  requestFeedback(BuildContext context){
+    debugPrint("feedback >> issue == $issue");
+    debugPrint("feedback >> contact == $contact");
+    debugPrint("feedback >> photo == $photoEntity");
+    if(issue.isEmpty){
+      ToastUtil.show('请填写反馈内容');
+      return;
+    }
+    // KeyboardUtils.hideKeyboard(context);
+    // Get.showLoading();
+    Future.delayed(const Duration(seconds: 2)).then((value){
+      // Get.dismiss();
+      ToastUtil.show('反馈成功');
+      Get.back();
+    });
+  }
+  
   @override
   void onInit() {
     super.onInit();
--- lib/app/styles/theme_constants.dart
+++ lib/app/styles/theme_constants.dart
@@ -4,127 +4,127 @@ class ThemeConstants {
   /// 主题色相关
   ///
   /// 主题色
-  static Color brandPrimary = Color(0xFF0984F9);
+  static const Color brandPrimary = Color(0xFF0984F9);
 
   /// 主题色按下效果
-  static Color brandPrimaryTap = Color(0x190984F9);
+  static const Color brandPrimaryTap = Color(0x190984F9);
 
   /// 成功色
-  static Color brandSuccess = Color(0xFF00AE66);
+  static const Color brandSuccess = Color(0xFF00AE66);
 
   /// 警告色
-  static Color brandWarning = Color(0xFFFAAD14);
+  static const Color brandWarning = Color(0xFFFAAD14);
 
   /// 失败色
-  static Color brandError = Color(0xFFFA3F3F);
+  static const Color brandError = Color(0xFFFA3F3F);
 
   /// 重要-多用于红点色
-  static Color brandImportant = Color(0xFFFA3F3F);
+  static const Color brandImportant = Color(0xFFFA3F3F);
 
   /// 重要数值色
-  static Color brandImportantValue = Color(0xFFFF5722);
+  static const Color brandImportantValue = Color(0xFFFF5722);
 
   /// 辅助色
-  static Color brandAuxiliary = Color(0xFF44C2FF);
+  static const Color brandAuxiliary = Color(0xFF44C2FF);
 
   /// 文本色相关
   ///
   /// 基础文字纯黑色
-  static Color colorTextBase = Color(0xFF222222);
+  static const Color colorTextBase = Color(0xFF222222);
 
   /// 基础文字重要色
-  static Color colorTextImportant = Color(0xFF666666);
+  static const Color colorTextImportant = Color(0xFF666666);
 
   /// 基础文字-反色
-  static Color colorTextBaseInverse = Color(0xFFFFFFFF);
+  static const Color colorTextBaseInverse = Color(0xFFFFFFFF);
 
   /// 辅助文字色
-  static Color colorTextSecondary = Color(0xFF999999);
+  static const Color colorTextSecondary = Color(0xFF999999);
 
   /// 失效或不可更改文字色
-  static Color colorTextDisabled = Color(0xFF999999);
+  static const Color colorTextDisabled = Color(0xFF999999);
 
   /// 文本框提示暗文文字色
-  static Color colorTextHint = Color(0xFFCCCCCC);
+  static const Color colorTextHint = Color(0xFFCCCCCC);
 
   /// 跟随主题色[brandPrimary]
-  static Color colorLink = Color(0xFF0984F9);
+  static const Color colorLink = Color(0xFF0984F9);
 
   /// 背景色相关
   ///
   /// 组件背景色
-  static Color fillBase = Color(0xFFFFFFFF);
+  static const Color fillBase = Color(0xFFFFFFFF);
 
   /// 页面背景色
-  static Color fillBody = Color(0xFFF8F8F8);
+  static const Color fillBody = Color(0xFFF8F8F8);
 
   /// 遮罩背景
-  static Color fillMask = Color(0x99000000);
+  static const Color fillMask = Color(0x99000000);
 
   /// 边框色
-  static Color borderColorBase = Color(0xFFF0F0F0);
+  static const Color borderColorBase = Color.fromARGB(255, 231, 231, 231);
 
   /// 分割线色
-  static Color dividerColorBase = Color(0xFFF0F0F0);
+  static const Color dividerColorBase = Color(0xFFF0F0F0);
 
   /// 文本字号
   ///
   /// 特殊数据展示，Bebas 数字字体，用于强吸引
-  static double fontSizeBebas = 28.0;
+  static const double fontSizeBebas = 28.0;
 
   /// 标题字体
   /// 名称/页面大标题
-  static double fontSizeHeadLg = 22.0;
+  static const double fontSizeHeadLg = 22.0;
 
   /// 标题字体
   /// 内容模块标题/一级标题
-  static double fontSizeHead = 18.0;
+  static const double fontSizeHead = 18.0;
 
   /// 子标题字体
   /// 标题/录入文字/大按钮文字/二级标题
-  static double fontSizeSubHead = 16.0;
+  static const double fontSizeSubHead = 16.0;
 
   /// 基础字体
   /// 内容副文本/普通说明文字
-  static double fontSizeBase = 14.0;
+  static const double fontSizeBase = 14.0;
 
   /// 辅助字体-普通
-  static double fontSizeCaption = 12.0;
+  static const double fontSizeCaption = 12.0;
 
   ///辅助字体-小
-  static double fontSizeCaptionSm = 11.0;
+  static const double fontSizeCaptionSm = 11.0;
 
   /// 圆角尺寸
-  static double radiusXs = 2.0;
-  static double radiusSm = 4.0;
-  static double radiusMd = 6.0;
-  static double radiusLg = 8.0;
+  static const double radiusXs = 2.0;
+  static const double radiusSm = 4.0;
+  static const double radiusMd = 6.0;
+  static const double radiusLg = 8.0;
 
   /// 边框尺寸
-  static double borderWidthSm = 0.5;
-  static double borderWidthMd = 1.0;
-  static double borderWidthLg = 2.0;
+  static const double borderWidthSm = 0.5;
+  static const double borderWidthMd = 1.0;
+  static const double borderWidthLg = 2.0;
 
   /// 水平间距
-  static double hSpacingXs = 8.0;
-  static double hSpacingSm = 12.0;
-  static double hSpacingMd = 16.0;
-  static double hSpacingLg = 20.0;
-  static double hSpacingXl = 24.0;
-  static double hSpacingXxl = 42.0;
+  static const double hSpacingXs = 8.0;
+  static const double hSpacingSm = 12.0;
+  static const double hSpacingMd = 16.0;
+  static const double hSpacingLg = 20.0;
+  static const double hSpacingXl = 24.0;
+  static const double hSpacingXxl = 42.0;
 
   /// 垂直间距
-  static double vSpacingXs = 4.0;
-  static double vSpacingSm = 8.0;
-  static double vSpacingMd = 12.0;
-  static double vSpacingLg = 14.0;
-  static double vSpacingXl = 16.0;
-  static double vSpacingXxl = 28.0;
+  static const double vSpacingXs = 4.0;
+  static const double vSpacingSm = 8.0;
+  static const double vSpacingMd = 12.0;
+  static const double vSpacingLg = 14.0;
+  static const double vSpacingXl = 16.0;
+  static const double vSpacingXxl = 28.0;
 
   /// 图标大小
-  static double iconSizeXxs = 8.0;
-  static double iconSizeXs = 12.0;
-  static double iconSizeSm = 14.0;
-  static double iconSizeMd = 16.0;
-  static double iconSizeLg = 32.0;
+  static const double iconSizeXxs = 8.0;
+  static const double iconSizeXs = 12.0;
+  static const double iconSizeSm = 14.0;
+  static const double iconSizeMd = 16.0;
+  static const double iconSizeLg = 32.0;
 }
