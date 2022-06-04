2496e4613ca09676c9ae2d01b1aad1da7a2748d2
Locki <lockiyang@qq.com>
2022-06-04 13:39:50 +0800

    remove job

--- lib/app/modules/app_job/data/models/index.dart
+++
@@ -1,7 +0,0 @@
-export 'user_info.dart' ; 
-export 'app_version.dart' ; 
-export 'login_info.dart' ; 
-export 'industry.dart' ; 
-export 'banner.dart' ; 
-export 'post_list.dart' ; 
-export 'post.dart' ; 
--- lib/app/modules/app_job/job_message/job_system_message/job_system_message_controller.dart
+++
@@ -1,6 +0,0 @@
-import 'package:get/get.dart';
-
-class JobSystemMessageController extends GetxController {
-  @override
-  void onClose() {}
-}
